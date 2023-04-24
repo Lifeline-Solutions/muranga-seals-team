9f95e751340df82b04b0aff66403620453c69eef
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-24 17:40:35 +0300

    Added fonts

--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -22,7 +22,7 @@ const Ticket = () => {
         <h1>Now Selling</h1>
       </div>
 
-      <div className="w-[90%] my-4 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
+      <div className="w-[90%] my-8 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
         <div className="flex">
           <img src={logo} alt="logo" className="w-[150px] h-[150px]" />
           <div className="flex flex-col justify-between ">
@@ -33,6 +33,7 @@ const Ticket = () => {
             </div>
           </div>
         </div>
+        <h1 className="text-red-500 flex text-5xl items-center">SOLD OUT</h1>
 
         <div className="flex flex-col gap-12 justify-between">
           <p className="bg-gray-200  flex items-center gap-2  h-[40px] p-8 shadow-2xl  text-start">
@@ -44,30 +45,7 @@ const Ticket = () => {
           </p>
         </div>
       </div>
-      <div className="w-[90%] my-4 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
-        <div className="flex">
-          <img src={logo} alt="logo" className="w-[150px] h-[150px]" />
-          <div className="flex flex-col justify-between ">
-            <h1>Muranga Seals Junior Team</h1>
-            <div className="flex flex-col">
-              <h1>Sun April 29- 6:00 pm</h1>
-              <h1 className="text-gray-500">Division 2</h1>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-12 justify-between">
-          <p className="bg-gray-200  flex items-center gap-2  h-[40px] p-8 shadow-2xl  text-start">
-            <span>General</span>
-            <span>Membership</span>
-          </p>
-          <p className="bg-[#F4E721] h-[40px] p-8 shadow-2xl  text-center flex items-center justify-center">
-            Buy Now
-          </p>
-        </div>
-      </div>
-
-      <div className="w-[90%] my-4 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
+      <div className="w-[90%] my-8 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
         <div className="flex">
           <img src={logo} alt="logo" className="w-[150px] h-[150px]" />
           <div className="flex flex-col justify-between ">
@@ -90,7 +68,31 @@ const Ticket = () => {
         </div>
       </div>
 
-      <div className="w-[90%] my-4 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
+      <div className="w-[90%] my-8 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
+        <div className="flex">
+          <img src={logo} alt="logo" className="w-[150px] h-[150px]" />
+          <div className="flex flex-col justify-between ">
+            <h1>Muranga Seals Junior Team</h1>
+            <div className="flex flex-col">
+              <h1>Sun April 29- 6:00 pm</h1>
+              <h1 className="text-gray-500">Division 2</h1>
+            </div>
+          </div>
+        </div>
+        <h1 className="text-red-500 flex text-5xl items-center">SOLD OUT</h1>
+
+        <div className="flex flex-col gap-12 justify-between">
+          <p className="bg-gray-200  flex items-center gap-2  h-[40px] p-8 shadow-2xl  text-start">
+            <span>General</span>
+            <span>Membership</span>
+          </p>
+          <p className="bg-[#F4E721] h-[40px] p-8 shadow-2xl  text-center flex items-center justify-center">
+            Buy Now
+          </p>
+        </div>
+      </div>
+
+      <div className="w-[90%] my-8 p-4 mx-auto border-t-8 border-yellow-200 bg-gray-100 text-3xl font-bold flex justify-between">
         <div className="flex">
           <img src={logo} alt="logo" className="w-[150px] h-[150px]" />
           <div className="flex flex-col justify-between ">
@@ -113,7 +115,7 @@ const Ticket = () => {
         </div>
       </div>
 
-      <div className="flex justify-between bg-gray-100 mx-auto w-[90%] p-4">
+      <div className="flex justify-between bg-gray-100 mx-auto w-[90%] my-12 p-4">
         <div className="flex flex-col gap-4 text-3xl">
           <h1 className="text-5xl font-bold">MURANG&apos;A SEALS MEMBERSHIP</h1>
           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,</p>
